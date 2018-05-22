c077e63b429db0262f282f7ad607d2da7c0196d8
Nathan <n.hatz@yahoo.com>
2018-05-22 13:48:52 -0500

    done for now

--- server/index.js
+++ server/index.js
@@ -1,16 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mc = require( `./controllers/messages_controller` );
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mc = require(`./controllers/messages_controller`);
+const createInitialSession = require(`${__dirname}/middlewares/session.js`);
+const filter = require(`${__dirname}/middlewares/filter.js`);
 
 const app = express();
+// TOP LEVEL MIDDLEWARE GETS HIT WITH EVERY REQUEST
+// REQUEST LEVEL MIDDLEWARE IS CALLED AS A SPECIFIC METHOD
+app.use(sessions());
+app.use(bodyParser.json());
+// takes in the location of our front end build folder
+// SPA. Use for production
+app.use(express.static(`${__dirname}/../build`));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUnitialized: false,
+    cookie: { maxAge: 100000 }
+  })
+);
 
-app.use( bodyParser.json() );
-app.use( express.static( `${__dirname}/../build` ) );
+app.use((req, res, next) => filter(req, res, next));
 
-app.post( "/api/messages", mc.create );
-app.get( "/api/messages", mc.read );
-app.put( "/api/messages", mc.update );
-app.delete( "/api/messages", mc.delete );
+app.use((req, res, next) => createInitialSession(req, res, next));
 
-const port = process.env.PORT || 3000
-app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
+// ENDPOINTS
+app.post("/api/messages", mc.create);
+app.get("/api/messages", mc.read);
+app.put("/api/messages", mc.update);
+app.delete("/api/messages", mc.delete);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
+});
---
+++ server/middlewares/session.js
@@ -0,0 +1,11 @@
+const notAllowed = ["poo", "butt"];
+
+module.exports = function(req, res, next) {
+  const { session, method } = req;
+  if (!session.user) {
+    session.user = {
+      messages: []
+    };
+  }
+  next();
+};
